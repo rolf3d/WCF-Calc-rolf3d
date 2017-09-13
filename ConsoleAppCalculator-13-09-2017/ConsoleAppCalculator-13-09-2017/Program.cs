4435266a1e727ec142e77cb7739f3f32fe0b6a4a
Rolf Petersen <rolf3d@gmail.com>
2017-09-13 11:22:00 +0200

    Lille ændring af service

--- ConsoleAppCalculator-13-09-2017/ConsoleAppCalculator-13-09-2017/Program.cs
+++ ConsoleAppCalculator-13-09-2017/ConsoleAppCalculator-13-09-2017/Program.cs
@@ -12,7 +12,13 @@ namespace ConsoleAppCalculator_13_09_2017
         {
             using (var client = new ServiceReferenceCalculator.Service1Client())
             {
-                Console.WriteLine(client.Add(2,2));
+                Console.WriteLine("Add " + client.Add(2,2));
+
+                Console.WriteLine("Multiply " + client.Multiply(2, 2));
+
+                Console.WriteLine("Subtract " + client.Subtract(2, 2));
+
+                Console.WriteLine("Divide " + client.Divide(2, 2));
             }
 
             Console.ReadLine();
