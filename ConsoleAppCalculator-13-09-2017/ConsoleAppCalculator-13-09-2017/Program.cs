a10354f0cb64d52baa4f34986a4253854f256796
Rolf Petersen <rolf3d@gmail.com>
2017-09-13 11:07:30 +0200

    4 Metoder til lommeregenr

---
+++ ConsoleAppCalculator-13-09-2017/ConsoleAppCalculator-13-09-2017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculator_13_09_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new ServiceReferenceCalculator.Service1Client())
+            {
+                Console.WriteLine(client.Add(2,2));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
